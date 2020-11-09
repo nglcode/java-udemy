cd040753e96836feab4c4b013ea961eff6b02f72
nglcode <nglcode@gmail.com>
2020-11-09 17:15:29 +0100

    first commit

---
+++ Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author agdev
+ */
+public class HolaMundo {
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo desde Java");
+        
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adiós";
+        System.out.println(miVariableCadena);
+    }
+}
