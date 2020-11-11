afc60c7fb46bfa4544845d93b6999e9e139afdbc
nglcode <nglcode@gmail.com>
2020-11-11 12:50:13 +0100

    Sobrecarga, paquetes, imports

---
+++ Fundamentos/Paquetes/src/test/TestUtileria.java
@@ -0,0 +1,13 @@
+
+package test;
+//import tech.nglcode.paquetes.Utileria;
+//import static tech.nglcode.paquetes.Utileria.imprimir;
+
+public class TestUtileria {
+    
+    public static void main(String[] args) {
+        
+        tech.nglcode.paquetes.Utileria.imprimir("Saludos");
+    }
+    
+}
