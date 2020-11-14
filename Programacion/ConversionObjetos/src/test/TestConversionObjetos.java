35a4960c066799d2336c9006ac114ff681f7ca1b
nglcode <nglcode@gmail.com>
2020-11-15 00:22:18 +0100

    conversion, instanceof

---
+++ Programacion/ConversionObjetos/src/test/TestConversionObjetos.java
@@ -0,0 +1,23 @@
+package test;
+
+import domain.Empleado;
+import domain.Escritor;
+import domain.TipoEscritura;
+
+
+public class TestConversionObjetos {
+    
+    public static void main(String[] args) {
+        
+        Empleado empleado;
+        
+        empleado = new Escritor("Juan", 5000, TipoEscritura.CLASICO);
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+        
+        Escritor escritor = (Escritor) empleado;
+        escritor.getTipoEscritura();
+        
+        
+    }
+    
+}
