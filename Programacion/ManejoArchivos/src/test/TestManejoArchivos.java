6da6fef6d3836269ff2f39400ac9c67a4540b580
Goncalves Sousa. Ángel Lorenzo (ADM) <adm.agoncalves@sanidad.msc>
2020-11-16 16:55:11 +0100

    manejo archivos

---
+++ Programacion/ManejoArchivos/src/test/TestManejoArchivos.java
@@ -0,0 +1,23 @@
+
+package test;
+
+import static manejoarchivos.ManejoArchivos.*;
+
+
+public class TestManejoArchivos {
+    
+    public static void main(String[] args) {
+        
+        var nombreArchivo = "prueba.txt";
+//        crearArchivo(nombreArchivo);
+        
+//        escribirArchivo(nombreArchivo, "Hola desde Java");
+//        anexarArchivo(nombreArchivo, "Esta es una nueva cadena");
+//        anexarArchivo(nombreArchivo, "Cadena de cierre.");
+
+          leerArchivo(nombreArchivo);
+        
+
+    }
+    
+}
