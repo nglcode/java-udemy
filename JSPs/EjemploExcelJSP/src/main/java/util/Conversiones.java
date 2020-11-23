839f76d8c54a3b5eaf70f7dffaf6a9feb4ff2936
Goncalves Sousa. Ángel Lorenzo (ADM) <adm.agoncalves@sanidad.msc>
2020-11-23 12:23:53 +0100

    EjemploExcelJSP

---
+++ JSPs/EjemploExcelJSP/src/main/java/util/Conversiones.java
@@ -0,0 +1,17 @@
+
+package util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Conversiones {
+    
+    private static final String FORMATO_FECHA = "dd-MM";
+    
+    public static String format(Date fecha) {
+        SimpleDateFormat formateador = new SimpleDateFormat(FORMATO_FECHA);
+        return formateador.format(fecha);
+    }
+    
+}
