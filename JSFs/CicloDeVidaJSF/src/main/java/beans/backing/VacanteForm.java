f1614ac117852f0923e6b973e3609da726a8e602
nglcode <nglcode@gmail.com>
2020-11-29 01:42:56 +0100

    Ciclo de vida JSF

---
+++ JSFs/CicloDeVidaJSF/src/main/java/beans/backing/VacanteForm.java
@@ -0,0 +1,37 @@
+
+package beans.backing;
+
+import beans.model.Candidato;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.apache.logging.log4j.*;
+
+
+@Named
+@RequestScoped
+public class VacanteForm {
+    
+    @Inject
+    private Candidato candidato;
+    
+    Logger log = LogManager.getRootLogger();
+
+    public Candidato getCandidato() {
+        return candidato;
+    }
+
+    public void setCandidato(Candidato candidato) {
+        this.candidato = candidato;
+    }
+    
+    public String enviar(){
+        if(this.candidato.getNombre().equals("Juan")){
+            log.info("Entrando al caso de exito.");
+            return "exito";
+        } else {
+            log.info("Entrando al caso de fallo.");
+            return "fallo";
+        }
+    }
+}
---
+++ JSFs/CicloDeVidaJSF/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,26 @@
+package beans.model;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.apache.logging.log4j.*;
+
+@Named
+@RequestScoped
+public class Candidato {
+    
+    private String nombre = "Introduce tu nombre";
+    
+    Logger log = LogManager.getRootLogger();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        log.info("Modificando la propiedad de nombre: " + this.nombre);
+    }
+    
+    
+    
+}
