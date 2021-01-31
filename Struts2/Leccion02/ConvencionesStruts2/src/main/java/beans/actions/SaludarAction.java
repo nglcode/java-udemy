34197d8dbbba808fb72c26f466ff7d2494bf9f15
nglcode <nglcode@gmail.com>
2021-01-31 19:59:41 +0100

    holamundo con convenciones sin struts.cml

---
+++ Struts2/Leccion02/ConvencionesStruts2/src/main/java/beans/actions/SaludarAction.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans.actions;
+
+import org.apache.logging.log4j.*;
+
+/**
+ *
+ * @author agdev
+ */
+public class SaludarAction {
+    
+    Logger log = LogManager.getLogger(SaludarAction.class);
+    
+    private String saludosAtr;
+    
+    public String execute(){
+        log.info("Ejecutando metodo execute desde struts2");
+        this.saludosAtr = "Hola desde Struts2 con convenciones";
+        return "exito";
+    }
+
+    public String getSaludosAtr() {
+        return saludosAtr;
+    }
+
+    public void setSaludosAtr(String saludosAtr) {
+        this.saludosAtr = saludosAtr;
+    }
+    
+    
+    
+}
