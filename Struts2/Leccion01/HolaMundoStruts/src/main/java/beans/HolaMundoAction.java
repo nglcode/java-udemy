3d01b9173633407ec5d3bcc9374b5d165ebdee13
nglcode <nglcode@gmail.com>
2021-01-31 19:26:16 +0100

    (desde NB) Primer proyecto con Struts2

---
+++ Struts2/Leccion01/HolaMundoStruts/src/main/java/beans/HolaMundoAction.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import org.apache.logging.log4j.*;
+
+/**
+ *
+ * @author agdev
+ */
+public class HolaMundoAction {
+    
+    Logger log = LogManager.getLogger(HolaMundoAction.class);
+    
+    private String saludosAtr;
+    
+    public String execute(){
+        log.info("Ejecutando metodo execute desde struts2");
+        this.saludosAtr = "Saludos desde Struts2";
+        return "exito";
+    }
+
+    public String getSaludosAtr() {
+        return saludosAtr;
+    }
+
+    public void setSaludosAtr(String saludosAtr) {
+        this.saludosAtr = saludosAtr;
+    }
+    
+    
+    
+}
