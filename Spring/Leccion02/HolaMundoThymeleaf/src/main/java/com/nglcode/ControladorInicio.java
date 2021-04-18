03bf560249b01f778ceccbad5fba4ec436dae89b
nglcode <nglcode@gmail.com>
2021-04-18 22:54:02 +0200

    HolaMundo con Thymeleaf intro mvc

--- Spring/Leccion02/HolaMundoThymeleaf/src/main/java/com/nglcode/ControladorInicio.java
+++ Spring/Leccion02/HolaMundoThymeleaf/src/main/java/com/nglcode/ControladorInicio.java
@@ -1,17 +1,51 @@
 package com.nglcode;
 
+import com.nglcode.domain.Persona;
+import java.util.ArrayList;
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
-    
+
+    @Value("${index.saludo}")
+    private String saludo;
+
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model model) {
+        var mensaje = "Mensaje con Thymeleaf";
+
+        var persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jperez@mail.com");
+        persona.setTelefono("852258654");
+
+        var persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("kgomez@mail.com");
+        persona2.setTelefono("654852159");
+        
+//        var personas = new ArrayList();
+//        personas.add(persona);
+//        personas.add(persona2);
+        
+        // otra forma de crear la lista
+        var personas = Arrays.asList(persona, persona2);
+//        var personas = new ArrayList();
+
         log.info("Ejecutando el controlador Spring MVC");
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
+//        model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
-    
+
 }
