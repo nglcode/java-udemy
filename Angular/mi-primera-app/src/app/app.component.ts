2148506dd1755929e30b3c30baccda61e63f254b
nglcode <nglcode@gmail.com>
2021-09-26 21:53:47 +0200

    creando los primeros componentes

--- Angular/mi-primera-app/src/app/app.component.ts
+++ Angular/mi-primera-app/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-primera-app';
+  titulo = 'Hola Mundo desde Angular';
+  saludo = 'Saludos desde Angular';
 }
+
--- Angular/mi-primera-app/src/app/app.module.ts
+++ Angular/mi-primera-app/src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { PersonasComponent } from './personas/personas.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PersonasComponent, PersonaComponent
   ],
   imports: [
     BrowserModule
